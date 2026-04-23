3405d18875d497c023e1a47be423655d62081efb
admin <admin@MacBook-Pro-admin.local>
2026-04-23 17:52:52 +0300

    CyberChat

--- main.py
+++ main.py
@@ -1,68 +1,37 @@
-from fastapi import FastAPI, WebSocket, Request
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+import os
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-clients = {}  # username -> websocket
-
+clients = {}
 
 @app.get("/")
-def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
+def home():
+    return FileResponse(os.path.join("templates", "index.html"))
 
 @app.websocket("/ws/{username}")
-async def ws(websocket: WebSocket, username: str):
+async def websocket_endpoint(websocket: WebSocket, username: str):
     await websocket.accept()
     clients[username] = websocket
-
-    # отправляем список пользователей всем
-    await broadcast_users()
 
     try:
         while True:
             data = await websocket.receive_json()
 
-            msg_type = data["type"]
+            msg = {
+                "from": username,
+                "to": data["to"],
+                "text": data["text"]
+            }
 
-            # 💬 сообщение
-            if msg_type == "message":
-                to = data["to"]
-                text = data["text"]
+            if data["to"] in clients:
+                await clients[data["to"]].send_json(msg)
 
-                message = {
-                    "type": "message",
-                    "from": username,
-                    "to": to,
-                    "text": text
-                }
+            await websocket.send_json(msg)
 
-                # личка
-                if to in clients:
-                    await clients[to].send_json(message)
-
-                # себе тоже
-                await websocket.send_json(message)
-
-            # 🔄 запрос пользователей
-            elif msg_type == "get_users":
-                await broadcast_users()
-
-    except:
-        if username in clients:
-            del clients[username]
-        await broadcast_users()
-
-
-async def broadcast_users():
-    users = list(clients.keys())
-
-    for ws in clients.values():
-        await ws.send_json({
-            "type": "users",
-            "users": users
-        })
+    except WebSocketDisconnect:
+        clients.pop(username, None)
