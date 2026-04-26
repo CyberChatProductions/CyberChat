96ab93f4219b011f0d5ab6ae7df644b2a80abe97
CyberChatProductions <shevelka.dan@gmail.com>
2026-04-27 00:05:33 +0300

    Update script.js

--- app/static/script.js
+++ app/static/script.js
@@ -1,16 +1,11 @@
-console.log("JS LOADED");
-
 let ws;
 let username;
 let currentUser = null;
+let users = [];
 
 function login() {
     username = document.getElementById("nameInput").value.trim();
-
-    if (!username) {
-        alert("Enter name");
-        return;
-    }
+    if (!username) return;
 
     const protocol = location.protocol === "https:" ? "wss" : "ws";
     ws = new WebSocket(`${protocol}://${location.host}/ws`);
@@ -21,34 +16,48 @@ function login() {
 
     ws.onmessage = (e) => {
         const [sender, msg] = e.data.split("|");
-        showMessage(sender, msg);
+        addMessage(sender, msg);
     };
 
     document.getElementById("login").style.display = "none";
-    document.getElementById("app").style.display = "block";
+    document.getElementById("app").style.display = "flex";
+}
+
+function addUser() {
+    const name = prompt("Username:");
+    if (!name || users.includes(name)) return;
+
+    users.push(name);
+
+    const div = document.createElement("div");
+    div.className = "user";
+    div.innerText = name;
+
+    div.onclick = () => {
+        currentUser = name;
+        document.getElementById("messages").innerHTML = "";
+    };
+
+    document.getElementById("users").appendChild(div);
 }
 
 function send() {
     const msg = document.getElementById("msgInput").value;
-
     if (!currentUser || !msg) return;
 
     ws.send(currentUser + "|" + msg);
+
+    addMessage(username, msg);
     document.getElementById("msgInput").value = "";
 }
 
-function showMessage(sender, msg) {
+function addMessage(sender, msg) {
     const box = document.getElementById("messages");
 
     const div = document.createElement("div");
+    div.className = "msg " + (sender === username ? "me" : "other");
 
-    if (sender === username) {
-        div.style.textAlign = "right";
-        div.innerHTML = msg;
-    } else {
-        div.style.textAlign = "left";
-        div.innerHTML = sender + ": " + msg;
-    }
+    div.innerText = sender === username ? msg : sender + ": " + msg;
 
     box.appendChild(div);
     box.scrollTop = box.scrollHeight;
