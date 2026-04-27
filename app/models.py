db149a9e6580c4b1d05c4c70a9038282be65e4e3
CyberChatProductions <shevelka.dan@gmail.com>
2026-04-27 17:03:09 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,18 +1,10 @@
-from sqlalchemy import Column, Integer, String, DateTime
-from datetime import datetime
+from sqlalchemy import Column, Integer, String
 from app.db import Base
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True, index=True)
 
 class Message(Base):
     __tablename__ = "messages"
 
-    id = Column(Integer, primary_key=True)
-    sender = Column(String, index=True)
-    receiver = Column(String, index=True)
+    id = Column(Integer, primary_key=True, index=True)
+    sender = Column(String)
+    receiver = Column(String)
     content = Column(String)
-    timestamp = Column(DateTime, default=datetime.utcnow)
