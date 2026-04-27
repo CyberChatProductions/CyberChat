f8545f2ccd049e072f397a55d3f3b40c9b3dd6ad
CyberChatProductions <shevelka.dan@gmail.com>
2026-04-27 17:03:30 +0300

    Update db.py

--- app/db.py
+++ app/db.py
@@ -4,9 +4,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 DATABASE_URL = "sqlite:///./chat.db"
 
 engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
