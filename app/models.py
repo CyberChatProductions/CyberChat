69bf06410cc9b04db847e8bd88f5665397e6a922
CyberChatProductions <shevelka.dan@gmail.com>
2026-04-27 18:50:58 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ from app.db import Base
 class Message(Base):
     __tablename__ = "messages"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     sender = Column(String)
     receiver = Column(String)
     content = Column(String)
@@ -14,6 +14,15 @@ class Message(Base):
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True)
     password = Column(String)
+
+
+class Device(Base):
+    __tablename__ = "devices"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    device_id = Column(String, unique=True)
+    user_agent = Column(String)
